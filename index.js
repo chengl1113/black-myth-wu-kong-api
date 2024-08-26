ad196b0cb173775c03e67caa941d3013c14a321e
cheng <chengl@bu.edu>
2024-08-26 13:13:30 -0400

    moved mongo uri to dotenv

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 // MongoDB Atlas connection
-mongoose.connect('mongodb+srv://chengl:Uilnhwms213!@blackmythapi.gecf6.mongodb.net/blackmyth?retryWrites=true&w=majority&appName=blackmythAPI', {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
